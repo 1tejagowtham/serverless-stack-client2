491dae0768a6a4f7064de40c29ce39af5deaed3b
1tejagowtham <1tejagowtham@gmail.com>
2019-10-19 03:50:42 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "teja-notes"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://3bdlxyw2h9.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_CTJ9YJDIU",
+    APP_CLIENT_ID: "29v7ttg0kgmosr4an28plrl3bv",
+    IDENTITY_POOL_ID: "us-east-2:77d80127-1c58-496f-b259-dbd3d4e12e69"
+  }
+};
